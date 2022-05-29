ce138ec4a6310e6ede0989cf605adc74f4214174
belulok_sebastian <belulok_sebastian@hotmail.com>
2022-05-29 14:23:38 +0800

    Added a private data for our bot token

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,10 @@
+import { config } from 'dotenv';
+config({ path: '.env' });
+
 import { Client, Intents } from 'discord.js';
 
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 bot.on('ready', ()=> console.log('The bot is up and running!'));
 
-bot.login('OTM0Njc4MTMxNDg5NTA1Mjgx.GHRTwZ.y0DCQuAXvrlQBRnPOP7Rt3AGfVNrtDLUSNgQoA');
+bot.login(process.env.BOT_TOKEN);
